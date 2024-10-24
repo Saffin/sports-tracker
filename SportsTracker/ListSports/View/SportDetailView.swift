619e56e8cb86bff4fe5fff77ebb8fd83eb5a4906
David Safarik <dsafarik@csas.cz>
2024-10-24 11:51:52 +0200

    detail view polished

--- SportsTracker/ListSports/View/SportDetailView.swift
+++ SportsTracker/ListSports/View/SportDetailView.swift
@@ -1,16 +1,45 @@
 import SwiftUI
 
+// just for navigation purposes
 struct SportDetailView: View {
     let sport: SportModel
+    
     var body: some View {
-        VStack {
-            Text(sport.name)
-                .font(.largeTitle)
-            Text(sport.location)
-                .font(.headline)
-            Text(sport.duration)
-                .font(.headline)
+        self.contentView
+        .padding()
+        .frame(maxWidth: .infinity)
+        .roundedCard()
+        .padding()
+        .navigationBarTitle("Record Details", displayMode: .inline)
+    }
+}
+
+private extension SportDetailView {
+    var contentView: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 24) {
+                HStack {
+                    Text(sport.name)
+                        .font(.system(size: 36, weight: .bold, design: .rounded))
+                        .frame(alignment: .leading)
+                        .padding(.bottom)
+                    Spacer()
+                    self.sportImage
+                }
+                HStack {
+                    Text(sport.location)
+                        .font(.system(size: 24, weight: .medium, design: .default))
+                        .foregroundColor(.secondary)
+                    Spacer()
+                    Text(sport.duration)
+                        .font(.system(size: 18, weight: .light, design: .monospaced))
+                }
+            }
+            Spacer()
         }
+    }
+    var sportImage: some View {
+        Image(systemName: sport.storage == .local ? "icloud.and.arrow.down" : "icloud.and.arrow.up")
     }
 }
 
@@ -24,4 +53,23 @@ struct SportDetailView: View {
             storage: .remote
         )
     )
+}
+
+
+// MARK: - Extensions
+extension View {
+    func roundedCard() -> some View {
+        self.modifier(RoundedCardModifier())
+    }
+}
+
+struct RoundedCardModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .background(
+                RoundedRectangle(cornerRadius: 25, style: .continuous)
+                    .fill(Color(UIColor.systemBackground))
+                    .shadow(color: Color.black.opacity(0.1), radius: 10, x: 0, y: 5)
+            )
+    }
 }
